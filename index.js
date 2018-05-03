d69345385ee2bb89cfb03f9f518cb5e7d0bf51b7
luca.stasio <luca.stasio@itresources.it>
2018-05-03 17:20:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * node-mu
+ * node.js minimalistic microservices framework on top of Express.js
+ * 
+ * Copyright(c) 2018 IT Resources s.r.l.
+ * Copyright(c) 2018 Luca Stasio <luca.stasio@itresources.it>
+ * 
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ * 
+ * index.js
+ */
+
+'use strict';
+
+const NodeMuLib = require('./lib');
+module.exports = NodeMuLib;
