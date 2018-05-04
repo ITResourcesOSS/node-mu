ba2bae1b991a6d0e8c463cff0e863c4159cbe86c
luca.stasio <luca.stasio@itresources.it>
2018-05-04 16:57:40 +0200

    AMQP connection fixs

--- examples/with-controller/simple-controller.js
+++ examples/with-controller/simple-controller.js
@@ -10,15 +10,36 @@ class SimpleController extends Controller {
     this.amqpPublisher = new AmqpPublisher({
       type: 'exchange',
       exchange: {
-        name: 'exchange-name',
-        route: 'route-name2'
+        name: 'uaa_events2',
+        route: 'uaa_new_user_route'
       }
     });
   }
 
-  info(req, res) {
-    this.logger.debug('[*] Request to get controller information');
-    res.json({ controller: 'SimpleController', version: '1.0' });
+  info(req, res, next) {
+    try {
+      this.logger.debug('[*] Request to get controller information');
+      
+
+      try {
+        this.amqpPublisher.publish({
+          type: 'new_user',
+          data: {
+            id: 12,
+            username: 'joshuagame'
+          }
+        });
+      } catch (err) {
+        this.logger.error(`error sending event: ${err}`);
+      }
+      
+
+      return res.json({ controller: 'SimpleController', version: '2.0' });
+    
+    } catch (err) {
+      return next(err);
+    }
+    
   }
 }
 
