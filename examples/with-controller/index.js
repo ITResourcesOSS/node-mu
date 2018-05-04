b828d4d8d4692d15d08e3a1e8be99380a19b2834
luca.stasio <luca.stasio@itresources.it>
2018-05-04 12:56:07 +0200

    Component base class to attach instance ids

--- examples/with-controller/index.js
+++ examples/with-controller/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Service } = require('../../lib');
+const { Service, AmqpPublisher } = require('../../lib');
 const SimpleRoute = require('./simple-route');
 
 Promise = require('bluebird');
@@ -14,13 +14,37 @@ class SimpleService extends Service {
     this.logger.info('setting up routes');
     const simpleRoute = new SimpleRoute();
     this.addRoute(simpleRoute);
+    
+    /*const evt = {
+      type: 'new_user',
+      data: {
+        id: 12,
+        username: 'joshuagame'
+      }
+    };
+    this.emit(evt);*/
   }
+
+  /*
+  emit(evt) {
+    this.amqpConnectionManager.channel.publish('uaa_events', 'uaa_new_user_route', new Buffer(JSON.stringify(evt)));
+  }
+  */
 }
 
 const start = async() => {
   try {
     console.log('Service initialization');
     const service = new SimpleService(__dirname);
+    service.on('amqp-connection-ok', () => {
+      const amqpPublisher = new AmqpPublisher({
+        type: 'exchange',
+        exchange: {
+          name: 'exchange-name',
+          route: 'route-name'
+        }
+      });
+    });
     await service.start();
   } catch(err) {
     throw err;
--- examples/with-controller/simple-controller.js
+++ examples/with-controller/simple-controller.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const { Controller } = require('../../lib');
+const { AmqpPublisher } = require('../../lib');
 
 class SimpleController extends Controller {
   constructor() {
     super();
     this.logger.info('[*] Simple Controller initialized');
+    this.amqpPublisher = new AmqpPublisher({
+      type: 'exchange',
+      exchange: {
+        name: 'exchange-name',
+        route: 'route-name2'
+      }
+    });
   }
 
   info(req, res) {
