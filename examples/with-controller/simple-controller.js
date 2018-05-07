106d68a4ac1de378e8744633547c8a7b2559f533
luca.stasio <luca.stasio@itresources.it>
2018-05-07 12:24:42 +0200

    EventsEmitter publish on AMQP exchange

--- examples/with-controller/simple-controller.js
+++ examples/with-controller/simple-controller.js
@@ -8,33 +8,17 @@ class SimpleController extends Controller {
   constructor() {
     super();
     this.logger.info('[*] Simple Controller initialized');
-    this.amqpPublisher = new AmqpPublisher({
-      type: 'exchange',
-      exchange: {
-        name: 'uaa_events',
-        route: 'uaa_new_user_route'
-      }
-    });
-
-    const conn = Hertzy.tune('conn');
-    conn.emit('conn:ok', { status: 'ok' });
   }
 
   async info(req, res, next) {
     try {
       this.logger.debug('[*] Request to get controller information');
 
-      try {
-        this.amqpPublisher.publish({
-          type: 'new_user',
-          data: {
-            id: 12,
-            username: 'joshuagame'
-          }
-        });
-      } catch (err) {
-        this.logger.error(`error sending event: ${err}`);
-      }
+      const apiEvents = Hertzy.tune('api-events');
+      apiEvents.emit('new_user', {
+        id: 122,
+        username: 'frank'
+      });
 
       return res.json({ controller: 'SimpleController', version: '2.0' });
 
